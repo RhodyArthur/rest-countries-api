a669d6e657e64ed4fc4753ab122432d95b52b179
RhodyArthur <rhodaarthur48@gmail.com>
2025-06-04 14:08:39 +0000

    feat: add loading indicator

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Data } from '../../services/data';
 import { DecimalPipe } from '@angular/common';
+import { Spinner } from "../spinner/spinner";
 
 @Component({
   selector: 'app-card',
-  imports: [DecimalPipe],
+  imports: [DecimalPipe, Spinner],
   templateUrl: './card.html',
   styleUrl: './card.sass'
 })
